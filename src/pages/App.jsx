d38b93088e57c69a119d66ac9aaef5686e9e40cf
RenanFernandoRuiz <renanfernandor908@gmail.com>
2023-08-11 10:21:12 -0300

    desenvolvido o profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import { useState } from "react";
 import Profile from "../components/profile/profile";
 
-export default function App(){
-    return(
-        <>
-        <Profile></Profile>
-        </>
+import "./App.css";
+
+import imgDarkMode from "../img/avatar.jpg";
+import imgLightMode from "../img/avatar-light.jpg";
+
+const App = () => {
+    const [isLightMode, setIsLightMode] = useState(false);
+
+    const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+    const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no modo Escuro"
+    return (
+        <div>
+            <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@Renan</Profile>
+        </div>
     );
 };
+
+export default App;

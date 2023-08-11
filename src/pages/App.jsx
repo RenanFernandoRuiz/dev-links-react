e7939745b8da6b5ab9239a554f508a55925bb627
RenanFernandoRuiz <renanfernandor908@gmail.com>
2023-08-11 11:06:07 -0300

    switch button ok

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Profile from "../components/profile/profile";
+import Switch from "../components/switch/switch";
 
 import "./App.css";
 
 import imgDarkMode from "../img/avatar.jpg";
 import imgLightMode from "../img/avatar-light.jpg";
+
 
 const App = () => {
     const [isLightMode, setIsLightMode] = useState(false);
@@ -12,8 +14,9 @@ const App = () => {
     const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
     const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no modo Escuro"
     return (
-        <div>
+        <div id="container">
             <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@Renan</Profile>
+            <Switch></Switch>
         </div>
     );
 };

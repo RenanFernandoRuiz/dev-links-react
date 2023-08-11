d8dda23cf0a55b95993b9d846750f7246ad9b18a
RenanFernandoRuiz <renanfernandor908@gmail.com>
2023-08-11 17:11:17 -0300

    projeto finalizado

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,6 +3,7 @@ import Profile from "../components/profile/profile";
 import Switch from "../components/switch/switch";
 import Links from "../components/links/links";
 import SocialLinks from "../components/socialLinks/socialLinks";
+import Footer from "../components/footer/footer";
 
 
 
@@ -10,18 +11,23 @@ import "./App.css";
 
 import imgDarkMode from "../img/avatar.jpg";
 import imgLightMode from "../img/avatar-light.jpg";
-import Footer from "../components/footer/footer";
 
 
 const App = () => {
     const [isLightMode, setIsLightMode] = useState(false);
+    
+    const toggleMode = () => {
+        setIsLightMode((prevMode) => !prevMode);
+    };
 
     const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
     const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no modo Escuro"
+    
     return (
+        <div id="App" className={isLightMode ? "light " : ""}>
         <div id="container">
             <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@Renan</Profile>
-            <Switch></Switch>
+            <Switch mode={toggleMode} infoButton={isLightMode}/>
             <div id="links">
                 <ul>
                     <Links link={"https://github.com"}>GitHub</Links>
@@ -39,6 +45,7 @@ const App = () => {
                 <SocialLinks href={"#"} icon={"logo-twitter"} />
             </div>
             <Footer link={"#"}>@Renan </Footer>
+        </div>
         </div>
     );
 };
